0171b6b13a34c050ba2a3696401dcbc0b8c35631
Brayan Espinoza <avantica06@utec.edu.pe>
2018-09-17 18:29:59 -0500

    Agrega data fake

--- process/js/app.js
+++ process/js/app.js
@@ -1,35 +1,62 @@
 var React = require ('react');
 var ReactDOM = require ('react-dom');
 
+
 var MainInterface = React.createClass({
 
   getInitialState : function () {
     return {
-      title : 'Appointments',
-      show : true,
+      apts : [
+  {
+    "petName": "Buffy",
+    "ownerName": "Hassum Harrod",
+    "aptDate": "2016-06-20 15:30",
+    "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
+  },
+  {
+    "petName": "Spot",
+    "ownerName": "Constance Smith",
+    "aptDate": "2016-06-24 08:30",
+    "aptNotes": "This German Shepherd is having some back pain"
+  },
+  {
+    "petName": "Goldie",
+    "ownerName": "Barot Bellingham",
+    "aptDate": "2016-06-22 15:50",
+    "aptNotes": "This Goldfish has some weird spots in the belly"
+  },
+  {
+    "petName": "Mitten",
+    "ownerName": "Hillary Goldwyn",
+    "aptDate": "2016-06-21 9:15",
+    "aptNotes": "Cat has excessive hairballs"
+  }
+   ]
     }
   },
-  
+
   render: function () {
 
-    var showtitle;
-
-    if (this.state.show) {
-      showTitle = 'New ';
-    }
-
-    var displayList = {
-      display: this.state.show ? 'block': 'none',
-      color : 'red',
-    }
     return (
       <div className = "interface">
-       <h1>{showTitle}{this.state.title}</h1>
-        <ul style = {displayList}>
-         <li>Buffy 3: 30</li>
-         <li>Spot 4: 30</li>
-        </ul>
-     </div>
+        <div className = "item-list media-list">
+          <ul className = "item-list media-list">
+            <li className = "pet-info media-body">
+              <div className = "pet-head">
+                <span className = "pet-name">{this.state.apts[0].petName}</span>
+                <span className = "apt-name pull-right">{this.state.apts[0].aptDate}</span>
+              </div>
+              <div className = "owner-name">
+                <span className = "label-item">Owner :</span>
+                {this.state.apts[0].ownerNames}
+              </div>
+              <div className = "apt-notes">
+                {this.state.apts[0].aptNotes}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     )
   }
 });
