5e2ebfe8a76afe03e896d699799b3a572e0e70ba
Brayan Espinoza <bjespinozah@gmail.com>
2018-09-18 18:06:46 -0500

    Add panel new appointment

--- process/js/app.js
+++ process/js/app.js
@@ -2,7 +2,8 @@ var React = require ('react');
 var ReactDOM = require ('react-dom');
 var _ = require('lodash');
 
-var AptList = require ('./AptList')
+var AptList = require ('./AptList');
+var AddAppointment = require('./AddAppointment');
 
 
 var MainInterface = React.createClass({
@@ -48,6 +49,7 @@ var MainInterface = React.createClass({
 
     return (
       <div className = "interface">
+          <AddAppointment />
           <ul className = "item-list media-list">
             {filteredApts}
           </ul>
